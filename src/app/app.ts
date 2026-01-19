bbfbec5da02f4922b8f3df810b1273f958212929
lorijr <jrsilva.lorivas@gmail.com>
2026-01-19 16:57:45 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignalsIntro } from "./signals-intro/signals-intro";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SignalsIntro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/signals-intro/signals-intro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signals-intro',
+  imports: [],
+  templateUrl: './signals-intro.html',
+  styleUrl: './signals-intro.css',
+})
+export class SignalsIntro {
+
+}
